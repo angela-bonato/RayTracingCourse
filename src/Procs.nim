9a0c495feccb510d1b675845c6d646d66090a06d
TommasoRovelli <tommaso.rovelli@yahoo.it>
2024-03-06 13:15:28 +0100

    Modified Procs.nim and corrected Types.nim

--- src/Procs.nim
+++ src/Procs.nim
@@ -3,13 +3,14 @@
 ]#
 
 import Types
-import std/sequtils
 
 ### Color type constructors ###
 
 proc newColor*(): Color =       
     #[Empty constructor, initialize all the variables to zero]#
-    result.r, result.g, result.b = 0.0
+    result.r = 0.0
+    result.g = 0.0
+    result.b = 0.0
     return result
 
 proc newColor*(r,g,b : float32): Color =
@@ -23,7 +24,8 @@ proc newColor*(r,g,b : float32): Color =
 
 proc newHdrImage*(): HdrImage = 
     #[Empty constructor, initialize all the variables to zero]#
-    result.height, result.width = 0
+    result.height = 0
+    result.width = 0
     result.pixels = newSeq[Color](0)
     return result
 
@@ -33,3 +35,46 @@ proc newHdrImage*(height,width : int): HdrImage =
     result.width = width
     result.pixels = newSeq[Color](height*width)
 
+### Color algebra ###
+
+func `+`*(c1,c2: Color) : Color =
+    #[Sum of two colors]#
+    result.r = c1.r + c2.r
+    result.g = c1.g + c2.g
+    result.b = c1.b + c2.b
+    return result
+
+func `*`*(c1,c2: Color) : Color =
+    #[Product of two colors]#
+    result.r = c1.r * c2.r
+    result.g = c1.g * c2.g
+    result.b = c1.b * c2.b
+    return result
+
+func `*`*(a: float32, c : Color) : Color =
+    #[Product of a scalar and a color]#
+    result.r = a * c.r
+    result.g = a * c.g
+    result.b = a * c.b 
+    return result
+
+### HdrImage pixel access ###
+
+proc valid_coordinates*(img: HdrImage, x,y: int) : bool = 
+    #[Check if the given coordinates are inside the image]#
+    return ( (x >= 0) and (x <= img.width) ) and
+           ( (y >= 0) and (y <= img.height) ) 
+
+proc pixel_offset*(img: HdrImage, x,y: int) : int = 
+    #[Give the linear position of a pixel, given its x,y]#
+    return y * img.width + x
+
+proc getPixel*(img: HdrImage, x,y: int) : Color = 
+    #[Get the color of the pixel at the coordinates x,y]#
+    assert img.valid_coordinates(x,y)
+    return img.pixels[img.pixel_offset(x,y)]
+
+proc setPixel*(img: HdrImage, x,y: int, new_color : Color) : void =
+    #[Set the color of the pixel at the coordinates x,y]#
+    assert img.valid_coordinates(x,y)
+    img.pixels[img.pixel_offset(x,y)] = new_color
