856f2c2d4dde4897745d6d11a22f3120189d6c94
Angela Bonato <angela.bonato1@studenti.unimi>
2024-04-10 16:00:07 +0200

    added transformation algebra to geometryalgebra.nim

--- src/geometryalgebra.nim
+++ src/geometryalgebra.nim
@@ -3,8 +3,10 @@
 import point
 import vector
 import normal
+import transformation
+import hommatrix
 
-# Functions for points and vectors
+# Algebra for points and vectors
 
 template define_3Dop(op: untyped, type1: typedesc, type2: typedesc, ret: typedesc) =
     proc op* (a: type1, b:type2) : ret =
@@ -13,14 +15,12 @@ template define_3Dop(op: untyped, type1: typedesc, type2: typedesc, ret: typedes
         result.y = op(a.y, b.y)
         result.z = op(a.z, b.z)
 
-define_3Dop(`+`, Vector, Point, Point)  # I am not so sure about it, but we can talk about it
+define_3Dop(`+`, Vector, Point, Point)
 define_3Dop(`+`, Point, Vector, Point)  # Equal to the one defined before but inverted for comodity
-#define_3Dop(`-`, Vector, Point, Point) # I don't think this is a proc we need (subtratting a point from a vector doesn't give nothing)
 define_3Dop(`-`, Point, Vector, Point)  
-#define_3Dop(`+`, Point, Point, Vector) # I don't think this is a proc we need (summing two point doesn't give nothing)
-define_3Dop(`-`, Point, Point, Vector)  #da controllare
+define_3Dop(`-`, Point, Point, Vector)
 
-proc point_to_vec*(p: Point) : Vector =   #è giusto??  credo di sì
+proc point_to_vec*(p: Point) : Vector =
     ## Gives the vector defined from the origin to the point taken as an argument
     result.x = p.x
     result.y = p.y
@@ -34,3 +34,53 @@ proc dot*(norm: Normal, vec: Vector) : float =
 proc dot*(vec: Vector, norm: Normal) : float =
     ## scalar product between vector and normal
     return norm.x * vec.x + norm.y * vec.y + norm.z * vec.z
+
+# Algebra usefull with transformations
+
+func `*`*(tr: Transformation, p1: Point) : Point =
+    ##  product between a transformation (4x4 matrix) and a point (3 coordinates and 1)
+    var 
+        p2 = newPoint()     # result of tr*p1
+        w = p1.x*tr.matrix.getElement(3, 0) + p1.y*tr.matrix.getElement(3, 1) + p1.z*tr.matrix.getElement(3, 2) + tr.matrix.getElement(3, 3)    # normalizing factor
+
+    p2.x = p1.x*tr.matrix.getElement(0, 0) + p1.y*tr.matrix.getElement(0, 1) + p1.z*tr.matrix.getElement(0, 2) + tr.matrix.getElement(0, 3)
+    p2.y = p1.x*tr.matrix.getElement(1, 0) + p1.y*tr.matrix.getElement(1, 1) + p1.z*tr.matrix.getElement(1, 2) + tr.matrix.getElement(1, 3)
+    p2.z = p1.x*tr.matrix.getElement(2, 0) + p1.y*tr.matrix.getElement(2, 1) + p1.z*tr.matrix.getElement(2, 2) + tr.matrix.getElement(2, 3)
+
+    if(w.almostEqual(1.0)):
+        return p2
+    else:
+        return newPoint(p2.x/w, p2.y/w, p3.z/w)
+
+func `*`*(tr: Transformation, v1: Vector) : Vector =
+    ##  product between a transformation (4x4 matrix) and a vector (3 coordinates and 0)
+    var 
+        v2 = newVector()     # result of tr*v1
+
+    v2.x = v1.x*tr.matrix.getElement(0, 0) + v1.y*tr.matrix.getElement(0, 1) + v1.z*tr.matrix.getElement(0, 2)
+    v2.y = v1.x*tr.matrix.getElement(1, 0) + v1.y*tr.matrix.getElement(1, 1) + v1.z*tr.matrix.getElement(1, 2)
+    v2.z = v1.x*tr.matrix.getElement(2, 0) + v1.y*tr.matrix.getElement(2, 1) + v1.z*tr.matrix.getElement(2, 2)
+
+    return v2
+
+func `*`*(tr: Transformation, v1: Vector) : Vector =
+    ##  product between a transformation (4x4 matrix) and a vector (3 coordinates and 0)
+    var 
+        v2 = newVector()     # result of tr*v1
+
+    v2.x = v1.x*tr.matrix.getElement(0, 0) + v1.y*tr.matrix.getElement(0, 1) + v1.z*tr.matrix.getElement(0, 2)
+    v2.y = v1.x*tr.matrix.getElement(1, 0) + v1.y*tr.matrix.getElement(1, 1) + v1.z*tr.matrix.getElement(1, 2)
+    v2.z = v1.x*tr.matrix.getElement(2, 0) + v1.y*tr.matrix.getElement(2, 1) + v1.z*tr.matrix.getElement(2, 2)
+
+    return v2
+
+func `*`*(tr: Transformation, n1: Normal) : Normal =
+    ##  product between a transformation (4x4 matrix) and a normal (3 coordinates and 0), similar to tr*vec but using the inverse traspose of tr
+    var 
+        n2 = newNormal()     # result of tr*n1
+
+    n2.x = n1.x*tr.inv_matrix.getElement(0, 0) + n1.y*tr.inv_matrix.getElement(1, 0) + n1.z*tr.inv_matrix.getElement(2, 0)
+    n2.y = n1.x*tr.inv_matrix.getElement(0, 1) + n1.y*tr.inv_matrix.getElement(1, 1) + n1.z*tr.inv_matrix.getElement(2, 1)
+    n2.z = n1.x*tr.inv_matrix.getElement(0, 2) + n1.y*tr.inv_matrix.getElement(1, 2) + n1.z*tr.inv_matrix.getElement(2, 2)
+
+    return n2
