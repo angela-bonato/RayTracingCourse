7726d3ad034c316f21e5c45da44d53282d4b2663
TommasoRovelli <tommaso.rovelli@yahoo.it>
2024-04-17 14:45:02 +0200

    Added FireRayProcs definitions

--- src/camera.nim
+++ src/camera.nim
@@ -2,13 +2,15 @@
 
 import transformation
 import ray
+import vector
+import point
 
 # Camera type declaration
 
 type Camera* = object 
     aspect_ratio* : float
     distance* : float
-    transform : Transformation
+    transform* : Transformation
 
 # Camera type constructors
 
@@ -28,10 +30,24 @@ proc newCamera*( aspect_ratio : float, distance: float, transform : Transformati
 
     return result
 
-# Fire Ray Procs definition (useful for the two way to see we want to implement)
+# Fire Ray Procs definition (useful for the two views we want to implement)
 
 type FireRayProcs* = proc (cam: Camera, u,v : float) : Ray {.closure.}
 
 # Fire Ray Procs definitions
 
-FireRayProc 
+proc fire_ray_orthogonal*(cam: Camera, u,v : float) : Ray =
+    ## Fire rays to obtain orthogonal projection 
+    var
+        origin = newPoint( -1.0, (1.0 - 2*u) * cam.aspect_ratio, 2*v-1 )
+        direction = newVector( 1.0, 0.0, 0.0 )
+
+    return newRay(origin, direction).transform( cam.transform )
+
+proc fire_ray_perspective*(cam: Camera, u,v : float) : Ray =
+    ## Fire rays to obtain prospective view
+    var 
+        origin = newPoint( -cam.distance, 0.0, 0.0)
+        direction = newVector( cam.distance, (1.0 - 2 * u) * cam.aspect_ratio, 2 * v - 1)
+
+    return newRay(origin, direction).transform( cam.transform )
