2ead6bba5059983d7033a6f5b200903ec6e0b4e1
Angela Bonato <angela.bonato1@studenti.unimi.it>
2024-05-22 10:55:15 +0200

    Wrong changes in renderprocs

--- src/renderprocs.nim
+++ src/renderprocs.nim
@@ -11,10 +11,11 @@ proc solverendproc*(hit: Option[HitRecord]): Color =
     ##Just a temporary proc which inherit from SolveRenderingProcs type, to be used in tests/test_5.nim
     return newColor(1.0, 2.0, 3.0)
 
-proc OnOffTracer*(hit : Option[HitRecord]) : Color =
+proc OnOffTracer*(hit : Option[HitRecord], back_color = newColor(0, 0, 0), hit_color = newColor(255, 255, 255)) : Color =
   ## This proc is used to determine the color of each pixel based on what the input ray hit
   if (hit.isNone) :
-    return newColor(0, 0, 0)  #The background will be black
+    return back_color  #The background will be black if default is used
   else:
-    return newColor(255, 255, 255)  #The spheres will be white
+    return hit_color  #The spheres will be white if default is used
 
+#Da sistemaree
